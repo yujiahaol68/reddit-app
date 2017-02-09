f934b59dd19162f37e4f7687bec12f7168757cd3
Tecker.Yu <767954505@qq.com>
2017-02-09 16:21:53 +0800

    control scroller in the router...not finish yet..

--- src/main.js
+++ src/main.js
@@ -80,9 +80,40 @@ Vue.use(VueLazyload, {
 
 Vue.use(VueRouter)
 //Routes Configuration
+
+// scrollBehavior:
+// - only available in html5 history mode
+// - defaults to no scroll behavior
+// - return false to prevent scroll
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    // savedPosition is only available for popstate navigations.
+    return savedPosition
+  } else {
+    const position = {}
+    // new navigation.
+    // scroll to anchor by returning the selector
+    if (to.hash) {
+      position.selector = to.hash
+    }
+    // check if any matched route config has meta that requires scrolling to top
+    if (to.matched.some(m => m.meta.scrollToTop)) {
+      // cords will be used if no selector is provided,
+      // or if the selector didn't match any element.
+      position.x = 0
+      position.y = 0
+    }
+    // if the returned position is falsy or an empty object,
+    // will retain current scroll position.
+    return position
+  }
+}
+
 const router = new VueRouter({
   mode: 'history',
-  routes
+  base: __dirname,
+  routes,
+  scrollBehavior
 })
 
 //For dev
